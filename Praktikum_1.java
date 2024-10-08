53f0caec86e0f6cb219f23b8001bb39cf7163817
Jefriivanka <148742610+Jefriivanka@users.noreply.github.com>
2024-10-08 23:21:04 +0700

    Add files via upload

---
+++ Praktikum_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum_1;
+
+/**
+ *
+ * @author USER
+ */
+public class Praktikum_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
